5ab2121e23645a196805d69b424441de0e2cbd14
jovimassimiliano <jovimassi@gmail.com>
2025-08-05 16:58:13 +0700

    feat: install vitest & msw

---
+++ vitest.setup.ts
@@ -0,0 +1,8 @@
+import { server } from '@/mocks/node'
+import "@testing-library/jest-dom/vitest";
+import { beforeAll, afterEach, afterAll } from 'vitest'
+
+ 
+beforeAll(() => server.listen())
+afterEach(() => server.resetHandlers())
+afterAll(() => server.close())
