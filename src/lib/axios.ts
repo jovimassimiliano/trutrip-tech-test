fcef40336cd1253186a52130474821ae2d7384f6
jovimassimiliano <jovimassi@gmail.com>
2025-08-07 00:29:29 +0700

    feat: added axios instance

---
+++ src/lib/axios.ts
@@ -0,0 +1,22 @@
+import { API_URL } from "@/constants/api";
+import axios from "axios";
+
+const axiosInstance = axios.create({
+  baseURL: API_URL,
+  timeout: 10000,
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+  },
+});
+
+axiosInstance.interceptors.request.use(
+  async (config) => {
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  },
+);
+
+export default axiosInstance;
